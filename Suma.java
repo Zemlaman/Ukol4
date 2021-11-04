8f87cec60cf96f1b3b33674dd9c45e873f957e2d
Matěj Žemlička <43809215+Zemlaman@users.noreply.github.com>
2021-11-04 15:35:25 +0100

    Add files via upload

---
+++ Suma.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Suma {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadejte cislo.");
+        int q = 0;
+        int w = 0;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        while (sc.hasNext()) {
+            list.add(sc.nextInt());
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            w += list.get(i);
+        }
+
+        double z = (double)w / list.size();
+        double avg = Math.ceil(z);
+        for (int a = 0; a < list.size(); a++) {
+            if(list.get(a) >= z){
+                q = q + list.get(a);
+            }
+        }
+        System.out.println(q);
+    }
+}
